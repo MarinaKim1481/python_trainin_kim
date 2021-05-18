9cc395e0791b1ddb5df8d91e90a3e5fe81614e17
MarinaKim1481 <marinavk1481@gmail.com>
2021-05-18 22:25:15 +0700

    Rename Methods (before fixture)

--- test/test_delete_first_group.py
+++ test/test_delete_first_group.py
@@ -1,4 +1,2 @@
 def test_delete_first_group(app):
-    app.sessionHelper.login("admin", password="secret")
     app.groupHelper.delete_first_group()
-    app.sessionHelper.close_auth()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,4 +1,2 @@
 def test_edit_contact(app):
-    app.sessionHelper.login("admin", password="secret")
     app.contactHelper.edit_first_contact()
-    app.sessionHelper.close_auth()
