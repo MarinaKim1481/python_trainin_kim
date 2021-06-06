ca5c8b358254598dd274cb537faf9f96e9972e83
MarinaKim1481 <marinavk1481@gmail.com>
2021-06-06 16:33:47 +0700

    test_count_group_list

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,10 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_list = app.groupHelper.get_group_list()
     app.groupHelper.add(Group(name="Test", header="Test_header", footer="Test_footer"))
+    new_list = app.groupHelper.get_group_list()
+    assert len(old_list) + 1 == len(new_list)
 
 def test_add_empty_group(app):
     app.groupHelper.add(Group(name="", header="", footer=""))
