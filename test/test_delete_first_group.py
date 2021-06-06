45f28bdff85ecc2c1d9ca4f930acd885f7df1a87
MarinaKim1481 <marinavk1481@gmail.com>
2021-06-06 16:38:03 +0700

    checking_count_group_list

--- test/test_delete_first_group.py
+++ test/test_delete_first_group.py
@@ -3,4 +3,7 @@ from model.group import Group
 def test_delete_first_group(app):
     if app.groupHelper.count() == 0:
         app.groupHelper.add(Group(name="Test_count", header="Test_header", footer="Test_footer"))
+    old_list = app.groupHelper.get_group_list()
     app.groupHelper.delete_first_group()
+    new_list = app.groupHelper.get_group_list()
+    assert len(old_list) - 1 == len(new_list)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 def test_edit_first_group_name(app):
+    old_list = app.groupHelper.get_group_list()
     app.groupHelper.edit_first_group_name(Group(name='New name'))
+    new_list = app.groupHelper.get_group_list()
+    assert len(old_list) == len(new_list)
 
 def test_edit_first_group_header(app):
+    old_list = app.groupHelper.get_group_list()
     app.groupHelper.edit_first_group_header(Group(header='New header'))
+    new_list = app.groupHelper.get_group_list()
+    assert len(old_list) == len(new_list)
