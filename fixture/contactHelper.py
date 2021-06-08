66f615a84bad54622d1672213b09ff773e18ffc2
MarinaKim1481 <marinavk1481@gmail.com>
2021-06-08 16:37:03 +0700

    sorted_groups_contacts_edit_del_1.1

--- fixture/contactHelper.py
+++ fixture/contactHelper.py
@@ -86,18 +86,36 @@ class ContactHelper:
         wd.switch_to_alert().accept()
         wd.find_element_by_link_text("home").click()
 
-    def edit_first_contact(self, new):
+    def edit_first_contact(self, new_contact_data):
         wd = self.app.wd
         self.app.open_homepage()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(new.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(new.lastname)
+        self.edit_contact(new_contact_data)
         wd.find_element_by_name("update").click()
         wd.find_element_by_link_text("home page").click()
+
+    def edit_contact(self, contact):
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("middlename", contact.middlename)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("nickname", contact.nickname)
+        self.change_field_value("photo_title", contact.photo_title)
+        self.change_field_value("company", contact.company)
+        self.change_field_value("address", contact.address)
+        self.change_field_value("home", contact.home_phone)
+        self.change_field_value("mobile", contact.mobile_phone)
+        self.change_field_value("fax", contact.fax)
+        self.change_field_value("work", contact.work_phone)
+        self.change_field_value("email", contact.email)
+        self.change_field_value("email2", contact.email2)
+        self.change_field_value("email3", contact.email3)
+        self.change_field_value("homepage", contact.homepage)
+        self.change_field_value("bday", contact.bday)
+        self.change_field_value("bmonth", contact.bmonth)
+        self.change_field_value("byears", contact.byears)
+        self.change_field_value("address2", contact.address2)
+        self.change_field_value("phone2", contact.phone2)
+        self.change_field_value("notes", contact.notes)
 
     def count(self):
         wd = self.app.wd
@@ -108,9 +126,16 @@ class ContactHelper:
         wd = self.app.wd
         self.app.wd.get("http://localhost/addressbook/index.php")
         contacts = []
-        for element in wd.find_elements_by_css_selector("//tr[@name='entry']"):
+        for element in wd.find_elements_by_xpath("//tr[@name='entry']"):
             firstname = element.find_element_by_css_selector("td:nth-child(3)").text
             lastname = element.find_element_by_css_selector("td:nth-child(2)").text
             id = element.find_element_by_name("selected[]").get_attribute("value")
             contacts.append(Contact(firstname=firstname, lastname=lastname, id=id))
         return contacts
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
